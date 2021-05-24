d8c9c1978fe6df03bbb507457d3c1b935dd15613
Stephanie Eordanidis <32005995+eordanis@users.noreply.github.com>
2021-05-24 09:15:08 -0400

    Update visual.py

--- visual.py
+++ visual.py
@@ -42,3 +42,37 @@ def display_stacking_metrics(directory=None, style=None, adjusted=None):
     # save metrics to .png for later use in pdf report
     plt.savefig(directory + 'classifier_metric_chart' + adjusted + '.png')
 
+
+def display_comparision_dataframe(directory=None):
+    if directory is None:
+        directory = default_dir
+        
+    df = pd.read_csv(directory + 'stacking_classifier_metrics.csv')
+    df1 = df.rename(columns={"Accuracy": "Acc_Standard", "Variance": "Var_Standard"})
+    df = pd.read_csv(directory + 'stacking_classifier_metrics_adjusted.csv')
+    df2 = df.rename(columns={"Accuracy": "Acc_Adjusted", "Variance": "Var_Adjusted"})
+    new_df = pd.merge(df1, df2,  how='left')
+    new_df = new_df.reindex(sorted(new_df.columns), axis=1)
+    new_df.set_index('Classifier')
+    display(new_df)
+    
+    
+def display_comparision_metrics(directory=None, style=None):
+    if directory is None:
+        directory = default_dir
+    
+    if style is None:
+        style = 'barh'
+        
+    df = pd.read_csv(directory + 'stacking_classifier_metrics.csv')
+    df1 = df.rename(columns={"Accuracy": "Acc_Standard", "Variance": "Var_Standard"})
+    df = pd.read_csv(directory + 'stacking_classifier_metrics_adjusted.csv')
+    df2 = df.rename(columns={"Accuracy": "Acc_Adjusted", "Variance": "Var_Adjusted"})
+    new_df = pd.merge(df1, df2,  how='left')
+    new_df = new_df.reindex(sorted(new_df.columns), axis=1)
+    df_plot = new_df[["Classifier","Acc_Standard","Acc_Adjusted","Var_Standard", "Var_Adjusted"]]
+    df_plot.set_index(["Classifier"],inplace=True)
+    df_plot.plot(kind=style', alpha=0.75, title='Classifier Metrics', figsize=(10, 10))
+    plt.xlabel("Values")
+    plt.show()       
+
